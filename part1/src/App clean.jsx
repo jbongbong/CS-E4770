bded8c6d83390487882fe4f87a1a3d74cb28bd0c
JB ONG <jbongryan@gmail.com>
2024-01-09 21:10:26 +0200

    finish reading part 1c

--- part1/src/App clean.jsx
+++ part1/src/App clean.jsx
@@ -2,37 +2,32 @@
 
 // Header renders the name of the course
 const Header = (props) => {
-  return(
-    <h1>
-      {props.course}
-    </h1>
+  return (
+    <h1>{props.course}</h1>
   )
 }
 
 // Part renders the name and number of exercises of one part
 const Part = (props) => {
-  return(
-    <p>
-      {props.part} {props.exercise}
-    </p>
+  return (
+    <p>{props.part} {props.exercise}</p>
   )
 }
 
 // Content renders the parts and their number of exercises
 const Content = (props) => {
-  return(
+  return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1}/>
-      <Part part={props.part2} exercise={props.exercises2}/>
-      <Part part={props.part3} exercise={props.exercises3}/>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
     </div>
   )
-
 }
 
 // Total renders the total number of exercises
 const Total = (props) => {
-  return(
+  return (
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
