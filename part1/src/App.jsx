a895798ebe8bb70e4c93ff6247daa94c084371da
JB ONG <jbongryan@gmail.com>
2024-01-10 17:07:20 +0200

    pre part1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -166,44 +166,138 @@ const Display = (props) => {
   )
 }
 
-const Button = (props) => {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   console.log('rendering with counter value', counter)
+//   const increaseByOne = () => {
+
+//     console.log('increasing, value before', counter)
+//     setCounter(counter + 1)
+//   }
+
+//   const decreaseByOne = () => { 
+
+//     console.log('decreasing, value before', counter)
+//     setCounter(counter - 1)
+//   }
+
+//   const setToZero = () => {
+
+//     console.log('resetting to zero, value before', counter)
+//     setCounter(0)
+//   }
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text="plus" />
+//       <Button onClick={setToZero} text="zero" />
+//       <Button onClick={decreaseByOne} text="minus" />
+//     </div>
+//   )
+// } 
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+//   const [total, setTotal] = useState(0)
+  
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     const updatedLeft = left + 1
+//     setLeft(updatedLeft)
+//     setTotal(updatedLeft + right) 
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     const updatedRight = right + 1
+//     setRight(updatedRight)
+//     setTotal(left + updatedRight)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+//       <Button handleClick={handleLeftClick} text='left' />
+//       <Button handleClick={handleRightClick} text='right' />
+//       {right}
+//       <History allClicks={allClicks} />
+//       <p>total {total}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const [value, setValue] = useState(10)
+
+
+//   const hello = (who) => () => {
+//     console.log('hello', who)
+//   }
+
+//   return (
+//     <div>
+//       {value}
+
+//       <button onClick={hello('world')}>world</button>
+//       <button onClick={hello('react')}>react</button>
+//       <button onClick={hello('function')}>function</button>
+//     </div>
+//   )
+// }
+
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [value, setValue] = useState(10)
 
-  console.log('rendering with counter value', counter)
-  const increaseByOne = () => {
-
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
+    // <div>
+    //   {value}
+    //   <button onClick={() => setToValue(1000)}>
+    //     thousand
+    //   </button>
+    //   <button onClick={() => setToValue(0)}>
+    //     reset
+    //   </button>
+    //   <button onClick={() => setToValue(value + 1)}>
+    //     increment
+    //   </button>
+    // </div>
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {value}
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
-} 
+}
 
 export default App
